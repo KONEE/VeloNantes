abf3a016c9d0f88603c544ee1377360bcd72d95c
KONEE <saadokone@gmail.com>
2019-04-19 12:08:19 +0200

    Add files via upload

---
+++ js/App.js
@@ -0,0 +1,7 @@
+//console.log("hello");
+let slid = new Sliders( document.querySelector("#mySlide"));
+slid.showSlides(3);
+
+//console.log(slides.showSlides(slideIndex));
+//showSlides(slideIndex);
+//let map = new Map("urlApi, cordonne");
--- js/diapo.js
+++ js/diapo.js
@@ -1,31 +1,43 @@
 
 // faire en objet plutard
-var slideIndex = 1;
-showSlides(slideIndex);
+class Sliders {
+       //var slideIndex = 1;
+        constructor(element) {
+           // var slideIndex = 1;
+          //slideIndex;
+          this.slideIndex = 3;
+          this.element = element;
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+        }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+      
+    
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
- 
+      // Next/previous controls
+      plusSlides(n) {
+          showSlides(slideIndex += n);
+      }
+
+      // Thumbnail image controls
+      currentSlide(n) {
+          showSlides(slideIndex = n);
+      }
+
+      showSlides(n) {
+          console.log("hello");
+            var i;
+            var slides = document.getElementsByClassName(this.element);
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {this.slideIndex = 1}
+            if (n < 1) {this.slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[this.slideIndex-1].style.display = "block";
+            dots[this.slideIndex-1].className += " active";
+        } 
+    }
+//var slider = new Sliders( "mySlide");
